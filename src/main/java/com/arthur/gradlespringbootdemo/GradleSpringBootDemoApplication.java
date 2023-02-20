e5e0de30f50bc401145042af431df34cd593d6eb
XSZ <1273343014@qq.com>
2023-02-21 00:08:27 +0800

    第一次提交

---
+++ src/main/java/com/arthur/gradlespringbootdemo/GradleSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.gradlespringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSpringBootDemoApplication.class, args);
+    }
+
+}
